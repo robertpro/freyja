ce4eec72b4900267b8c053da001f6b54ccd50df0
Jose Roberto Meza Cabrera <romeza@mx1.ibm.com>
2016-06-26 20:41:40 -0500

    Finished upload videos and ip to Dropbox

--- main.py
+++ main.py
@@ -1,20 +1,68 @@
 #!/usr/bin/env python
 
+from argparse import Namespace, ArgumentParser
+import os
+
 import updown
-from argparse import Namespace
+from network import get_public_ip
 
+HOME = os.path.expanduser('~')
 
-LOCAL_UPLOAD_FOLDER = '~/camera_data'
+LOCAL_UPLOAD_FOLDER = HOME + '/camera_data'
 DROPBOX_FOLDER = '/camera_data'
+LOCAL_IP_FILE = HOME + '/camera_data/ip'
+DROPBOX_IP_FILE = DROPBOX_FOLDER + '/ip'
+ACCESS_TOKEN = ''
+
+
+def upload_videos():
+    args = Namespace(default=False, folder=DROPBOX_FOLDER, no=False, rootdir=LOCAL_UPLOAD_FOLDER,
+                     token=ACCESS_TOKEN,
+                     yes=True)
+    updown.main(args)
+
+
+def upload_ip():
+    try:
+        local_ip = open(LOCAL_IP_FILE, 'r').read()
+    except IOError:
+        if not os.path.exists(LOCAL_UPLOAD_FOLDER):
+            os.mkdir(LOCAL_UPLOAD_FOLDER)
+        open(LOCAL_IP_FILE, 'w')
+        local_ip = ''
+    public_ip = get_public_ip()
+
+    if local_ip != public_ip:
+        with open(LOCAL_IP_FILE, 'w') as f:
+            f.write(public_ip)
+        updown.TransferFile(ACCESS_TOKEN).upload_file(LOCAL_IP_FILE, DROPBOX_IP_FILE)
 
 
 def main():
     """Upload """
-    access_token = ''
-    args = Namespace(default=False, folder=DROPBOX_FOLDER, no=False, rootdir=LOCAL_UPLOAD_FOLDER, token=access_token,
-                     yes=True)
+    args = get_args()
 
-    updown.main(args)
+    if args.upload_videos:
+        upload_videos()
+    if args.upload_ip:
+        upload_ip()
+
+
+def get_args():
+    parser = ArgumentParser(description='Upload all videos saved at {0} to Dropbox.'.format(LOCAL_UPLOAD_FOLDER))
+    parser.add_argument('-u', '--upload-videos', required=False, default=False, action='store_true')
+    parser.add_argument('-i', '--upload-ip', help='Upload ip file only', required=False, default=False,
+                        action='store_true')
+
+    args = parser.parse_args()
+
+    if sum([bool(b) for b in (args.upload_ip, args.upload_videos)]) > 1:
+        print('At most one of --upload-videos, --upload-ip is allowed')
+        exit(2)
+
+    return args
+
+
 
 if __name__ == '__main__':
     main()
--- updown.py
+++ updown.py
@@ -250,5 +250,14 @@ def stopwatch(message):
         print('Total elapsed time for %s: %.3f' % (message, t1 - t0))
 
 
-if __name__ == '__main__':
-    main()
+class TransferFile(object):
+    def __init__(self, access_token):
+        self.access_token = access_token
+
+    def upload_file(self, file_from, file_to):
+        """upload a file to Dropbox using API v2
+        """
+        dbx = dropbox.Dropbox(self.access_token)
+
+        with open(file_from, 'rb') as f:
+            dbx.files_upload(f, file_to)
