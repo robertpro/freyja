ad2ab281bb43c064dbef55ea1de5c5070411de3c
Jose Roberto Meza Cabrera <romeza@mx1.ibm.com>
2016-07-01 23:39:28 -0500

    Improved camera.sh script

--- bin/camera.sh
+++ bin/camera.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 USER="$1"
 PYTHON="/home/$USER/venvs/camera/bin/python"
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. ; pwd )"
 
 if test -z $USER; then 
     echo "No user given"
     exit 1
 fi
 
-if [ "$USER" == "pi" ]; then
-    cd /home/$USER/camera/
-else
-    cd ..
-fi
+cd $BASEDIR
 
 $PYTHON main.py -i
 $PYTHON main.py -u
